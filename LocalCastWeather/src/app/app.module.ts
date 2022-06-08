2efc165ccdb1b50ac3cb788027453b7c26411b12
vyakovleva <89025209+vyakovleva@users.noreply.github.com>
2022-06-08 20:09:48 +0300

    weather project

--- LocalCastWeather/src/app/app.module.ts
+++ LocalCastWeather/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
